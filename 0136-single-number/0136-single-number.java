bacd39cfa0a9715d641551fdcfb54f1e441880fc
SALONI KUMARI <Salonikumri1101@gmail.com>
2025-12-30 12:43:03 +0530

    Time: 13 ms (20.43%), Space: 46.7 MB (79.17%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+
+   HashMap<Integer,Integer> map=new HashMap<>();
+   for(int i =0; i<nums.length;i++){
+    if(map.containsKey(nums[i])){
+        map.put(nums[i],map.get(nums[i])+1);
+    }else{
+         map.put(nums[i],1) ;
+    }
+    
+   }
+
+
+for(Integer ans:map.keySet()){
+if(map.get(ans)==1)
+return ans;
+}
+
+return -1;
+
+        
+    }
+}
