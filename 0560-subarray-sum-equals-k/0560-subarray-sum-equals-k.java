1ceece291eae01b8e4c445767b0ce3b3ae679722
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-03-20 16:59:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,32 +2,36 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         
 
-        int ps[]=new int[nums.length];
-        ps[0]=nums[0];
+        // int ps[]=new int[nums.length];
+        // ps[0]=nums[0];
 
-        for(int i =1;i<nums.length;i++){
-            ps[i]=ps[i-1]+nums[i];
-        }
+        // for(int i =1;i<nums.length;i++){
+        //     ps[i]=ps[i-1]+nums[i];
+        // }
+
+
+        
          
 HashMap<Integer,Integer> m= new HashMap<>();
+m.put(0,1);
          int count =0;
-        // int maxLen =0;
+        int sum=0;
         for(int i =0;i<nums.length;i++){
-         
-int val =ps[i]-k;
-if(val==0){
-    count+=1;
-    //maxLen=Math.max(len,maxLen);
-}
+         sum+=nums[i];
+int val =sum-k;
+// if(val==0){
+//     count+=1;
+    
+// }
 
 if(m.containsKey(val)){
-    //m.put(ps[i],m.getOrDefault(ps[i],0)+1);
+  
     count+=m.get(val);
-    // maxLen=Math.max(len,maxLen);
+    
 }
 
 
-m.put(ps[i],m.getOrDefault(ps[i],0)+1);
+m.put(sum,m.getOrDefault(sum,0)+1);
 
         }
 
