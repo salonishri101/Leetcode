94b4b6e433cf2bda2501267f6e042d6aea3d9fc5
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-04-03 16:29:51 +0530

    Time: 0 ms (100%), Space: 45 MB (6.96%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+
+            
+int i =0,j = nums.length-1;
+int ans=nums.length;
+     while(i<=j){
+        int mid=i+(j-i)/2;
+          
+          if(nums[mid]>=target){
+            ans=mid;
+            j=mid-1;
+          }else if(nums[mid]<target){
+            i=mid+1;
+          }
+
+
+     }
+
+
+
+return ans;
+
+
+    }
+}
