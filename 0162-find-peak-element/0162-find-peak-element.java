840648c815ba287faecf5aa66c8fc2e383839918
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-04-04 15:20:03 +0530

    Time: 0 ms (100%), Space: 44.1 MB (65.4%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+
+             if (nums.length == 1) return 0;
+
+          if (nums[0] > nums[1]) return 0;
+        if (nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;  
+
+   int i =1,j=nums.length-2;  
+
+
+
+while(i<=j){
+int mid=i+(j-i)/2;
+
+if(nums[mid-1]<nums[mid] && nums[mid+1]<nums[mid]){
+    return mid;
+}else if(nums[mid-1]<nums[mid]){
+i=mid+1;
+}else{
+    j=mid-1;
+}
+
+}
+
+return -1;
+
+    }
+}
