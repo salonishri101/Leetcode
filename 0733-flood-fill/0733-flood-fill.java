7eba18dd97cab6c056eeb4a8f0ff11a80bd630b6
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-08-04 21:35:35 +0530

    Time: 2 ms (11.24%), Space: 47 MB (25.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,70 @@
 class Solution {
 
- public void help(int[][] image, int r, int c, int orgcolor,int newcolor){
-      if(r<0 || c<0 || r>=image.length || c>=image[0].length  || image[r][c]!=orgcolor || image[r][c]==newcolor ){
-        return ;
-      }
-      //vis[r][c]=true;->replace with image[r][c]=newcolor
-      image[r][c]=newcolor;
+class Pair{
 
-
-          
- help(image,r,c+1,orgcolor,newcolor);
- help(image,r,c-1,orgcolor,newcolor);
- help(image,r+1,c,orgcolor,newcolor);
- help(image,r-1,c,orgcolor,newcolor);
- }
-
-
+    int first;
+    int sec;
+    Pair(int first,int sec ){
+        this.first=first;
+        this.sec=sec;
+    }
+}
 
 
 
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        //int orgcolor=image[sr][sc];
-       // boolean vis[][]= new boolean[image.length];
-         help(image,sr,sc,image[sr][sc],color);
-         return image;
+        
+
+        int orgcol = image[sr][sc];
+
+        // Same color hai toh kuch karna hi nahi
+        if (orgcol == color) {
+            return image;
+        }
+
+       
+        boolean vis[][]=new boolean[image.length][image[0].length];
+        Queue<Pair> q = new LinkedList<>();
+    
+
+        q.add(new Pair(sr,sc));
+        
+        while(q.size()>0){
+            Pair curr=q.poll();
+            int i=curr.first;
+            int j=curr.sec;
+                 
+                  image[i][j]=color;
+
+
+            if(i-1>=0 && (image[i-1][j]==orgcol) && !vis[i-1][j]){
+                vis[i-1][j]=true;
+                image[i-1][j]=color;
+                q.add(new Pair(i-1,j));
+            }
+
+            if(j-1>=0 && (image[i][j-1]==orgcol) && !vis[i][j-1]){
+                vis[i][j-1]=true;
+                image[i][j-1]=color;
+                q.add(new Pair(i,j-1));
+            }
+
+            if(i+1<image.length && (image[i+1][j]==orgcol) && !vis[i+1][j]){
+                vis[i+1][j]=true;
+                image[i+1][j]=color;
+                q.add(new Pair(i+1,j));
+            }
+
+            if(j+1<image[0].length && (image[i][j+1]==orgcol) && !vis[i][j+1]){
+                vis[i][j+1]=true;
+                image[i][j+1]=color;
+                q.add(new Pair(i,j+1));
+            }
+        }
+
+
+      return image;
+
     }
 }
