878e1ba2f1a1364fb0a6cc041b4cc9d99ea84c33
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-08-16 22:57:36 +0530

    Time: 7 ms (29.05%), Space: 114.9 MB (52.03%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+
+        for (int x : stones) {
+            if (x % 3 == 0) c0++;
+            else if (x % 3 == 1) c1++;
+            else c2++;
+        }
+
+        if (c1 == 0 && c2 == 0) {
+            return false;
+        }
+
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        return Math.abs(c1 - c2) > 2;
+    }
+}
