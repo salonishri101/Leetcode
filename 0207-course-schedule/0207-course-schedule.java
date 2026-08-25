56307974cd8898c82872d1d614f5bfa67bd0c1ff
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-08-25 22:50:34 +0530

    Time: 7 ms (56.37%), Space: 46.9 MB (78.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,58 +1,48 @@
 class Solution {
 
-public boolean isCycle(int node,ArrayList<ArrayList<Integer>> graph,boolean vis[] ,boolean stack[] ){
-    stack[node]=true;
-    vis[node]=true;
-
-
-    for(int neigh:graph.get(node)){
-        if(stack[neigh]){
-            return true;
-        }
-
-        if(!vis[neigh] && isCycle(neigh,graph,vis,stack)){
-           return true;
-        }
-    }
-stack[node]=false;
-return false;
-
-
-}
-
-
-
-
-
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-         boolean vis[] = new boolean[numCourses];
-         boolean stack[] = new boolean[numCourses];
+        
+         Queue<Integer> q = new LinkedList<>();
 
         for(int i =0;i<numCourses;i++){
             graph.add(new ArrayList<>());
         }
-
+         int indegree[]= new int[numCourses];
 
         for(int[] i:prerequisites){
             int a =i[0];
             int b =i[1];
             graph.get(b).add(a);
+            indegree[a]+=1;
         }
+     
+
+     for(int i =0;i<numCourses;i++){
+           if(indegree[i]==0){
+            q.add(i);
+           }
+     }
        
+       
+       int count=0;
+    while(!q.isEmpty()){
+        int curr= q.poll();
+         count++;
 
-       for(int i =0;i<numCourses;i++){
-        if(!vis[i]){
+         for(int neigh:graph.get(curr)){
+           indegree[neigh]--;
+           if(indegree[neigh]==0){
+            q.add(neigh);
+           }
+         }
 
-            if(isCycle(i,graph,vis,stack)){
-                return false;
-            }
-        }
-       }
-return true;
+
+    }
+
+    return count==numCourses;
 
     }
 }
