6d74e5adcf4dce10d69c8217de5cdee60dfe058d
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-09-08 20:20:00 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countCommas(int n) {
+        
+
+
+
+int total =0;
+int a =n-999;
+if(a>0) total+=a;
+
+
+// if(n<1000) return 0;
+
+return total;
+
+
+
+
+// return -1;
+
+    }
+}
