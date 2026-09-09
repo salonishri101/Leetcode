287d0bbe5af24cb4dcb31314bf9c2a0cb2ed01ae
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-09-09 16:10:52 +0530

    Time: 11 ms (17.84%), Space: 46.3 MB (83.1%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,75 +1,82 @@
 class Solution {
-
-   class Info{
-    int node;
-    int cost;
-    int stops;
-    Info(int node,int cost,int stops){
-        this.node = node;
-        this.cost=cost;
-        this.stops=stops;
-    }
-   }
-
-
-
-
-
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
 
 
-
-        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-
-        for(int i =0;i<n;i++){
-            adj.add(new ArrayList<>());
+      class Pair{
+        int node;
+        int dist;
+        int stops;
+        Pair(int node,int dist,int stops){
+            this.node = node;
+            this.dist=dist;
+            this.stops=stops;
         }
-
-       for(int[] flight:flights){
-        int u =flight[0];
-        int v =flight[1];
-        int cost=flight[2];
-        adj.get(u).add(new int[]{v,cost});
-
-       }
-
-        
-     PriorityQueue<Info> pq = new PriorityQueue<>((a,b)->a.cost-b.cost);
-     int dist[][]= new int[k+2][n];
-      for(int i =0;i<=k+1;i++){
-        Arrays.fill(dist[i],Integer.MAX_VALUE);
-      }
-      dist[0][src]=0;
-
-     pq.add(new Info(src,0,0));
-     while(!pq.isEmpty()){
-        Info curr=pq.remove();
-        int currNode=curr.node;
-        int currCost=curr.cost;
-        int currStop=curr.stops;
-        if(currStop>k){
-          continue;
-        }
-
-        for(int i =0;i<adj.get(currNode).size();i++){
-            int[] neigh=adj.get(currNode).get(i);
-        int neighNode=neigh[0];
-        int neighCost=neigh[1];
-          if(currCost+neighCost<dist[currStop+1][neighNode]){
-              dist[currStop+1][neighNode]=currCost+neighCost;
-              pq.add(new Info(neighNode,dist[currStop+1][neighNode],currStop+1));
-            }
+      }  
 
 
-        }
-     }
+ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
 
- int ans=Integer.MAX_VALUE;
-    for(int i=0;i<=k+1;i++){
-        ans=Math.min(ans,dist[i][dst]);
+for(int i =0;i<n;i++){
+    graph.add(new ArrayList<>());
+
+}
+
+for(int edge[]:flights){
+    int u =edge[0];
+    int v = edge[1];
+    int wt =edge[2];
+
+    graph.get(u).add(new int[]{v,wt});
+}
+
+PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> a.dist-b.dist);
+pq.add(new Pair(src,0,0));
+int dist[][]= new int[n][k+2];
+
+for(int i = 0; i < n; i++) {
+    Arrays.fill(dist[i], Integer.MAX_VALUE);
+}
+
+dist[src][0] = 0;
+
+
+while(!pq.isEmpty()){
+
+    Pair curr=pq.poll();
+    int currNode = curr.node;
+    int currStops=curr.stops;
+    int currDist=curr.dist;
+
+    
+
+if(currNode==dst) return currDist;
+
+    // if(currStops>k) continue;
+    if(currStops == k + 1) continue;
+
+    
+
+for(int neigh[]:graph.get(currNode)){
+    int neighNode=neigh[0];
+    int neighDist=neigh[1];
+
+// pq.add(new Pair(neighNode,currDist+neighDist,currStops+1));
+int newDist=currDist+neighDist;
+if(newDist < dist[neighNode][currStops + 1]) {
+    dist[neighNode][currStops + 1] = newDist;
+    pq.add(new Pair(neighNode, newDist, currStops + 1));
+}
+    
+}
+
+    
+    
+    
     }
-    return ans==Integer.MAX_VALUE?-1:ans;
+
+
+return -1;
+
 
     }
 }
