0f1b100f409e823a3ea5e93c77554822e2e7fecf
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-09-13 17:28:49 +0530

    Time: 47 ms (15.23%), Space: 46.7 MB (79.76%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,40 +1,34 @@
 class Solution {
 
-public void dfs(boolean vis[],int curr,ArrayList<ArrayList<Integer>> adj){
-    vis[curr]=true;
-    for(int neigh:adj.get(curr)){
-        if(!vis[neigh]){
-            dfs(vis,neigh,adj);
+
+void dfs(int[][] stones,int i,boolean vis[]){
+    vis[i]=true;
+    for(int j =0;j<stones.length;j++){
+        if(!vis[j] && (stones[i][0] == stones[j][0] ||
+                 stones[i][1] == stones[j][1])){
+            dfs(stones,j,vis);
         }
     }
 }
 
     public int removeStones(int[][] stones) {
-        int n = stones.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        boolean vis[]= new boolean[n];
 
-        for(int i =0;i<n;i++){
-            adj.add( new ArrayList<>());
-        }
 
-        for(int i =0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(stones[i][0]==stones[j][0] ||stones[i][1]==stones[j][1] ){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-int count=0;
-            for(int i =0;i<n;i++){
-                if(!vis[i]){
-                    dfs(vis,i,adj);
-                    count++;
-                }
-                
-            }
+      int n =stones.length;
+      int m =stones[0].length;  
+boolean[] vis = new boolean[n];
 
-        return n-count;
+
+int compo=0;
+for(int i =0;i<n;i++){
+    if(!vis[i]){
+        dfs(stones,i,vis);
+        compo++;
+    }
+}
+
+return n -compo;
+
+
     }
 }
