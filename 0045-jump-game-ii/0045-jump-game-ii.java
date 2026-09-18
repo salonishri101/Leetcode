18bf3577d14e388f46f2e7fbf4fc0d913fdd4b58
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-09-19 01:14:01 +0530

    Time: 1 ms (99.8%), Space: 47.2 MB (57.77%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+
+int jumps=0;
+int farthest=0;
+int currEnd =0;
+
+
+for(int i =0;i<nums.length-1;i++){
+
+
+    farthest=Math.max(farthest,i+nums[i]);
+
+    if(i ==currEnd){
+        currEnd =farthest;
+        jumps++;
+    }
+}
+
+
+
+
+
+return jumps;
+
+
+    }
+}
